5653690838ea28423309c4c78ce51434d5723d6f
arslanashraf006 <arslanashraf7864@gmail.com>
2020-11-25 17:21:01 +0500

    lec#57//texts and Icons

--- lib/Input_Page.dart
+++ lib/Input_Page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'Repeate_contanier.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'RepeateTextIcon.dart';
 
 enum Gender {
   male,
