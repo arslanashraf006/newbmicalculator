16ec37c5dbea546899d36c08e5520a34b1347601
arslanashraf006 <arslanashraf7864@gmail.com>
2020-11-25 18:25:55 +0500

    lec#68//How to move from one screen to other

--- lib/Input_Page.dart
+++ lib/Input_Page.dart
@@ -3,6 +3,7 @@ import 'Repeate_contanier.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'RepeateTextIcon.dart';
 import 'constent_variable.dart';
+import 'RoundIcon.dart';
 
 // enum Gender {
 //   male,
@@ -30,101 +31,6 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI'),
       ),
-
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          RoundIcon(
-            icondata: FontAwesomeIcons.minus,
-            onPress: () {
-              setState(() {
-                slider_weight--;
-              });
-            },
-          ),
-          SizedBox(width: 5),
-          RoundIcon(
-            icondata: FontAwesomeIcons.plus,
-            onPress: () {
-              setState(() {
-                slider_weight++;
-              });
-            },
-          ),
-        ],
-      )
-      ],
-    ),
-    colors: Color(0xFF1D1E33),
-    ),
-    ),
-    Expanded(
-    child: Repeate_contanier(
-    cardWidge: Column(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-    Text('AGE', style: ConstentTextLabel),
-    Text(sliderage.toString(), style: ConstentTextLabe2),
-    Row(
-    mainAxisAlignment: MainAxisAlignment.center,
-    children: [
-    RoundIcon(
-    icondata: FontAwesomeIcons.minus,
-    onPress: () {
-    setState(() {
-    sliderage--;
-    });
-    },
-    ),
-    SizedBox(width: 5),
-    RoundIcon(
-    icondata: FontAwesomeIcons.plus,
-    onPress: () {
-    setState(() {
-    sliderage++;
-    });
-    },
-    ),
-    ],
-    )
-    ],
-    ),
-    colors: Color(0xFF1D1E33),
-    ),
-    ),
-    ],
-    ),
-    ),
-    Container(
-    color: Colors.red,
-    margin: EdgeInsets.only(top: 10),
-    height: 80.0,
-    )
-    ],
-    ),
-    );
-  }
-}
-
-class RoundIcon extends StatelessWidget {
-  RoundIcon({@required this.icondata, this.onPress});
-  final IconData icondata;
-  final Function onPress;
-  @override
-  Widget build(BuildContext context) {
-    return RawMaterialButton(
-      child: Icon(
-        icondata,
-        color: Colors.black,
-      ),
-      onPressed: onPress,
-      elevation: 6,
-      constraints: BoxConstraints.tightFor(height: 45.0, width: 45.0),
-      shape: CircleBorder(),
-      fillColor: Colors.white,
-    );
-  }
-}
       body: Column(
         children: [
           Expanded(
