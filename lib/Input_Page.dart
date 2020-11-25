4987ca7c07b6c6ab55933fd9638458cd8279e898
arslanashraf006 <arslanashraf7864@gmail.com>
2020-11-25 18:21:09 +0500

    lec#67//Floating action button in flutter//custom creation

--- lib/Input_Page.dart
+++ lib/Input_Page.dart
@@ -30,16 +30,101 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI'),
       ),
-      cardWidge: Column(
+
+      Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text(
-            'WEGHT',
-            style: ConstentTextLabel,
+          RoundIcon(
+            icondata: FontAwesomeIcons.minus,
+            onPress: () {
+              setState(() {
+                slider_weight--;
+              });
+            },
           ),
-          Text(slider_weight.toString(), style: ConstentTextLabe2),
+          SizedBox(width: 5),
+          RoundIcon(
+            icondata: FontAwesomeIcons.plus,
+            onPress: () {
+              setState(() {
+                slider_weight++;
+              });
+            },
+          ),
         ],
+      )
+      ],
+    ),
+    colors: Color(0xFF1D1E33),
+    ),
+    ),
+    Expanded(
+    child: Repeate_contanier(
+    cardWidge: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    Text('AGE', style: ConstentTextLabel),
+    Text(sliderage.toString(), style: ConstentTextLabe2),
+    Row(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+    RoundIcon(
+    icondata: FontAwesomeIcons.minus,
+    onPress: () {
+    setState(() {
+    sliderage--;
+    });
+    },
+    ),
+    SizedBox(width: 5),
+    RoundIcon(
+    icondata: FontAwesomeIcons.plus,
+    onPress: () {
+    setState(() {
+    sliderage++;
+    });
+    },
+    ),
+    ],
+    )
+    ],
+    ),
+    colors: Color(0xFF1D1E33),
+    ),
+    ),
+    ],
+    ),
+    ),
+    Container(
+    color: Colors.red,
+    margin: EdgeInsets.only(top: 10),
+    height: 80.0,
+    )
+    ],
+    ),
+    );
+  }
+}
+
+class RoundIcon extends StatelessWidget {
+  RoundIcon({@required this.icondata, this.onPress});
+  final IconData icondata;
+  final Function onPress;
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(
+        icondata,
+        color: Colors.black,
       ),
+      onPressed: onPress,
+      elevation: 6,
+      constraints: BoxConstraints.tightFor(height: 45.0, width: 45.0),
+      shape: CircleBorder(),
+      fillColor: Colors.white,
+    );
+  }
+}
       body: Column(
         children: [
           Expanded(
